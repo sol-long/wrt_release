44f2aa3acd54533ca2b146945bdc1cb419d5e0b0
sollong <79252851+sol-long@users.noreply.github.com>
2026-05-16 18:15:50 +0800

    switch: replace bandix-plus with bandix package, luci, and i18n

--- wrt_core/modules/feeds.sh
+++ wrt_core/modules/feeds.sh
@@ -29,8 +29,6 @@ update_feeds() {
 
     append_feed_if_missing "$FEEDS_PATH" "openwrt_bandix" "src-git openwrt_bandix https://github.com/timsaya/openwrt-bandix.git;main"
     append_feed_if_missing "$FEEDS_PATH" "luci_app_bandix" "src-git luci_app_bandix https://github.com/timsaya/luci-app-bandix.git;main"
-    append_feed_if_missing "$FEEDS_PATH" "openwrt_bandix_plus" "src-git openwrt_bandix_plus https://github.com/timsaya/openwrt-bandix-plus.git;main"
-    append_feed_if_missing "$FEEDS_PATH" "luci_app_bandix_plus" "src-git luci_app_bandix_plus https://github.com/timsaya/luci-app-bandix-plus.git;main"
     append_feed_if_missing "$FEEDS_PATH" "luci_app_argon_config" "src-git luci_app_argon_config https://github.com/jerrykuku/luci-app-argon-config.git;master"
 
     if [ ! -f "$BUILD_DIR/include/bpf.mk" ]; then
